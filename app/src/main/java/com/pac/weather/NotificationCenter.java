fd828d18d706615e57277e15a7980b6b49855070
roodabeh <roodabesafavi@yahoo.com>
2020-03-29 15:37:17 +0430

    dispatchqueue & notificationcenter

---
+++ app/src/main/java/com/pac/weather/NotificationCenter.java
@@ -0,0 +1,37 @@
+package com.pac.weather;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationCenter {
+    private List<Observer> observers = new ArrayList<>();
+    private static NotificationCenter notificationCenter;
+
+    public static NotificationCenter getInstance(){
+        if(notificationCenter == null)
+            notificationCenter = new NotificationCenter();
+        return notificationCenter;
+    }
+
+    public interface Observer {
+        void update();
+    }
+
+    public void dataLoaded(){
+        notifyObservers();
+    }
+
+    public void register(Observer observer){
+        observers.add(observer);
+    }
+
+    public void unRegister(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(){
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
