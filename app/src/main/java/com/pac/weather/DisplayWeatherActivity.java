c28eec37810fc602d805202bf19743c1e974fff0
Zahra Fazel <zfazel1379@gmail.com>
2020-04-09 23:26:04 +0430

    Fix day info bug.

--- app/src/main/java/com/pac/weather/DisplayWeatherActivity.java
+++ app/src/main/java/com/pac/weather/DisplayWeatherActivity.java
@@ -72,8 +72,9 @@ public class DisplayWeatherActivity extends AppCompatActivity implements Notific
                     Date date = new Date((controller.getTime() + i * 86400) * 1000);
                     Calendar cal = Calendar.getInstance(TimeZone.getTimeZone(controller.getTimezone()));
                     cal.setTime(date);
-                    day[i] = cal.get(Calendar.DAY_OF_WEEK_IN_MONTH) + ", " + cal.get(Calendar.MONTH)
-                        + " " + cal.get(Calendar.DAY_OF_MONTH);
+                    day[i] = cal.get(Calendar.DAY_OF_WEEK_IN_MONTH) + ", " +
+                            getMonthName(cal.get(Calendar.MONTH))
+                        + " " + getWeekDayName(cal.get(Calendar.DAY_OF_MONTH));
                     summary[i] = weather.getSummery();
                     icon[i] = getResources().getIdentifier(weather.getIcon().replace('-','_')
                                                             ,"drawable", getPackageName());
@@ -111,5 +112,40 @@ public class DisplayWeatherActivity extends AppCompatActivity implements Notific
     {
         super.onDestroy();
         notificationCenter.unRegister(this);
+    }
+
+    private static String getMonthName(int month)
+    {
+        switch (month)
+        {
+            case 0: return "January";
+            case 1: return "February";
+            case 2: return "March";
+            case 3: return "April";
+            case 4: return "May";
+            case 5: return "June";
+            case 6: return "July";
+            case 7: return "August";
+            case 8: return "September";
+            case 9: return "October";
+            case 10: return "November";
+            case 11: return "December";
+            default:return "";
+        }
+    }
+
+    private static String getWeekDayName(int weekDay)
+    {
+        switch (weekDay)
+        {
+            case 0: return "Monday";
+            case 1: return "Tuesday";
+            case 2: return "Wednesday";
+            case 3: return "Thursday";
+            case 4: return "Friday";
+            case 5: return "Saturday";
+            case 6: return "Sunday";
+            default:return "";
+        }
     }
 }
