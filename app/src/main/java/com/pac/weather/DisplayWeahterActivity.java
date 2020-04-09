4b7f540e73ac7d1ffe03397d73dcaab34d37fc56
Zahra Fazel <zfazel1379@gmail.com>
2020-04-09 17:38:57 +0430

    First activity is completed

--- app/src/main/java/com/pac/weather/DisplayWeahterActivity.java
+++ app/src/main/java/com/pac/weather/DisplayWeahterActivity.java
@@ -17,28 +17,33 @@ public class DisplayWeahterActivity extends AppCompatActivity implements Notific
     private String center;
     private ArrayList<Weather> dailyForecast;
 
-    public String getCenter() {
+    public String getCenter()
+    {
         return center;
     }
 
-    public ArrayList<Weather> getDailyForecast() {
+    public ArrayList<Weather> getDailyForecast()
+    {
         return dailyForecast;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         notificationCenter.register(this);
 
-        setContentView(R.layout.activity_display_weahter);
+        setContentView(R.layout.weather_list);
         dailyForecast = new ArrayList<>();
 
         Intent intent = getIntent();
         String msgKey = getString(R.string.msg_inflater_key);
         center = intent.getStringExtra(msgKey).replace("[", "").replace("]", "");
-        controller.dispatchQueue.postRunnable(new Runnable() {
+        controller.dispatchQueue.postRunnable(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 String url = getString(R.string.DarkSky_baseUrl) + center;
                 controller.makeRequest(DisplayWeahterActivity.this, url, "skyDark");
             }
@@ -48,18 +53,27 @@ public class DisplayWeahterActivity extends AppCompatActivity implements Notific
 
 
     @Override
-    public void update() {
-        runOnUiThread(new Runnable() {
+    public void update()
+    {
+        runOnUiThread(new Runnable()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 //TODO: create recycler view for list representation
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(dailyForecast.get(0).getSummery());
+//                TextView textView = findViewById(R.id.textView);
+//                textView.setText(dailyForecast.get(0).getSummery());
+                System.out.println(dailyForecast.get(0).getSummery());
+                System.out.println(dailyForecast.get(0).getWindSpeed());
+                System.out.println(dailyForecast.get(0).getHumidity());
+                System.out.println(dailyForecast.get(0).getPressure());
+                System.out.println(dailyForecast.get(0).getTemperature());;
             }
         });
     }
 
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         super.onDestroy();
         notificationCenter.unRegister(this);
     }
--- app/src/main/java/com/pac/weather/MainActivity.java
+++ app/src/main/java/com/pac/weather/MainActivity.java
@@ -8,23 +8,29 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements NotificationCenter.Observer {
+public class MainActivity extends AppCompatActivity implements NotificationCenter.Observer
+{
     private NotificationCenter notificationCenter = NotificationCenter.getInstance();
     private Controller controller = Controller.getInstance(notificationCenter);
 
     ArrayList<City> cities = new ArrayList<>();
 
-    public ArrayList<City> getCities() {
+    public ArrayList<City> getCities()
+    {
         return cities;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         notificationCenter.register(this);
 
@@ -32,59 +38,90 @@ public class MainActivity extends AppCompatActivity implements NotificationCente
 
 
         final EditText searchBox = findViewById(R.id.search_box);
-        searchBox.addTextChangedListener(new TextWatcher() {
+        searchBox.addTextChangedListener(new TextWatcher()
+        {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
-            public void afterTextChanged(Editable editable) {
-                if (searchBox.getText().length() != 0) {
-                    controller.dispatchQueue.postRunnable(new Runnable() {
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable)
+            {
+                if (searchBox.getText().length() != 0)
+                {
+                    controller.dispatchQueue.postRunnable(new Runnable()
+                    {
                         @Override
-                        public void run() {
-                            String url = getString(R.string.base_url) + searchBox.getText().toString() + getString(R.string.request_format) + getString(R.string.MapBox_token);
+                        public void run()
+                        {
+                            String url = getString(R.string.base_url) +
+                                    searchBox.getText().toString() + getString(R.string.request_format) +
+                                    getString(R.string.MapBox_token);
                             controller.makeRequest(MainActivity.this, url, "MapBox");
                         }
                     });
                 }
             }
+
+
         });
 
-        final TextView tempTextView = findViewById(R.id.temp_text_view);
-        tempTextView.setOnClickListener(new View.OnClickListener() {
+//        final TextView tempTextView = findViewById(R.id.temp_text_view);
+//        tempTextView.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                sendMessageToDisplayActivity(tempTextView.getText().toString());
+//            }
+//        });
+
+        final ListView list = findViewById(R.id.list);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                sendMessageToDisplayActivity(tempTextView.getText().toString());
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id)
+            {
+                sendMessageToDisplayActivity(cities.get(position).getCenter());
             }
         });
     }
 
 
     @Override
-    public void update() {
-        runOnUiThread(new Runnable() {
+    public void update()
+    {
+        runOnUiThread(new Runnable()
+        {
             @Override
-            public void run() {
-                //TODO: create recycler view for list representation
-                TextView textView = findViewById(R.id.temp_text_view);
-                textView.setText(cities.get(0).getName());
+            public void run()
+            {
+                String[] mainTitle = new String[cities.size()];
+                String[] subtitle = new String[cities.size()];
+                int i = 0;
+                for( City city : cities )
+                {
+                    mainTitle[i] = city.getName();
+                    subtitle[i] = city.getPlace();
+                    i++;
+                }
+                CityListAdapter adapter = new CityListAdapter(MainActivity.this, mainTitle, subtitle);
+                ListView list = findViewById(R.id.list);
+                list.setAdapter(adapter);
             }
         });
     }
 
-    protected void onDestroy() {
+    protected void onDestroy()
+    {
         super.onDestroy();
         notificationCenter.unRegister(this);
     }
 
-    public void sendMessageToDisplayActivity(String cityName) {
+    public void sendMessageToDisplayActivity(String cityName)
+    {
         String center = "";
 
         for (City city : cities)
